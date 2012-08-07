744bb42199f037ab9b3f3d76442f86baae150a7e
Adrian L Lange <adrianlund@gmail.com>
2012-08-07 23:55:17 +0200

    Less taints and tracking

--- Talentless.lua
+++ Talentless.lua
@@ -1,69 +1,57 @@
-
 local Talentless = CreateFrame('Frame', 'Talentless', UIParent, 'UIDropDownMenuTemplate')
 Talentless:SetScript('OnEvent', function(self, event, ...) self[event](self, event, ...) end)
-Talentless:RegisterEvent('PLAYER_LOGIN')
 Talentless:RegisterEvent('ADDON_LOADED')
 
-local function TabClick(self, button, down)
+local function OnClick(self, button)
 	if(button == 'RightButton') then
-		Talentless.group = self:GetID()
+		CloseDropDownMenus()
 		ToggleDropDownMenu(1, nil, Talentless, self, 0, 0)
 	end
-
-	PlayerSpecTab_OnClick(self, button, down)
 end
 
-local function MenuClick(self, name)
-	TalentlessDB[Talentless.group] = name
+local function SelectClick(self, name)
+	TalentlessDB[PlayerTalentFrame.talentGroup] = name
 end
 
 local function Initialize()
 	local info = UIDropDownMenu_CreateInfo()
 	for index = 1, GetNumEquipmentSets() do
 		local name, icon = GetEquipmentSetInfo(index)
-		info.text = string.format('|T%s:26|t %s', icon, name)
-		info.func = MenuClick
+		info.text = string.format('|T%s:18|t %s', icon, name)
+		info.func = SelectClick
 		info.arg1 = name
-		info.checked = TalentlessDB[Talentless.group] == name
+		info.checked = TalentlessDB[PlayerTalentFrame.talentGroup] == name
 		UIDropDownMenu_AddButton(info)
 	end
 end
 
-function Talentless:ADDON_LOADED(event, addon)
-	if(addon ~= 'Blizzard_TalentUI') then return end
-	self:UnregisterEvent(event)
+function Talentless:ADDON_LOADED(event, name)
+	if(name == 'Blizzard_TalentUI') then
+		TalentlessDB = TalentlessDB or {}
 
-	PlayerSpecTab1:RegisterForClicks('LeftButtonUp', 'RightButtonUp')
-	PlayerSpecTab1:SetScript('OnClick', TabClick)
-	PlayerSpecTab2:RegisterForClicks('LeftButtonUp', 'RightButtonUp')
-	PlayerSpecTab2:SetScript('OnClick', TabClick)
+		self:RegisterEvent('ACTIVE_TALENT_GROUP_CHANGED')
+		self:UnregisterEvent(event)
 
-	self.displayMode = 'MENU'
-	self.initialize = Initialize
-end
+		PlayerSpecTab1:RegisterForClicks('LeftButtonUp', 'RightButtonUp')
+		PlayerSpecTab2:RegisterForClicks('LeftButtonUp', 'RightButtonUp')
+		hooksecurefunc('PlayerSpecTab_OnClick', OnClick)
 
-function Talentless:PLAYER_LOGIN(event)
-	TalentlessDB = TalentlessDB or {}
-
-	self.currentGroup = GetActiveTalentGroup()
-	self:RegisterEvent('ACTIVE_TALENT_GROUP_CHANGED')
-	self:UnregisterEvent(event)		
-end
-
-function Talentless:ACTIVE_TALENT_GROUP_CHANGED()
-	if(self.currentGroup ~= GetActiveTalentGroup()) then
-		self:RegisterEvent('UNIT_SPELLCAST_STOP')
+		self.relativePoint = 'TOPRIGHT'
+		self.displayMode = 'MENU'
+		self.initialize = Initialize
 	end
 end
 
-function Talentless:UNIT_SPELLCAST_STOP(event)
-	local talentGroup = GetActiveTalentGroup()
-	self:UnregisterEvent(event)
-	self.currentGroup = talentGroup
+function Talentless:ACTIVE_TALENT_GROUP_CHANGED()
+	self:RegisterEvent('UNIT_SPELLCAST_STOP')
+end
 
-	for index = 1, GetNumEquipmentSets(index) do
+function Talentless:UNIT_SPELLCAST_STOP(event)
+	self:UnregisterEvent(event)
+
+	for index = 1, GetNumEquipmentSets() do
 		local name = GetEquipmentSetInfo(index)
-		if(TalentlessDB[talentGroup] == name) then
+		if(TalentlessDB[GetActiveTalentGroup()] == name) then
 			return EquipmentManager_EquipSet(name)
 		end
 	end

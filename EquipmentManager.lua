4e75a0143cd61ac77229b354518921f07ba67881
Adrian L Lange <mail@p3lim.net>
2017-01-22 09:21:42 +0100

    The bug with EquipmentManager_EquipSet is fixed on the PTR

--- EquipmentManager.lua
+++ EquipmentManager.lua
@@ -1,31 +1,33 @@
 -- http://www.wowinterface.com/forums/showthread.php?t=54889
 
-function EquipmentManager_EquipSet(name)
-	if(EquipmentSetContainsLockedItems(name) or UnitCastingInfo('player')) then
-		UIErrorsFrame:AddMessage(ERR_CLIENT_LOCKED_OUT, 1, 0.1, 0.1, 1)
-		return
-	end
+if(select(4, GetBuildInfo()) ~= 70200) then
+	function EquipmentManager_EquipSet(name)
+		if(EquipmentSetContainsLockedItems(name) or UnitCastingInfo('player')) then
+			UIErrorsFrame:AddMessage(ERR_CLIENT_LOCKED_OUT, 1, 0.1, 0.1, 1)
+			return
+		end
 
-	-- BUG: legion legendaries will halt the set equipping if the user is swapping
-	-- different slotted legendaries beyond the 1/2 equipped limit
-	local locations = GetEquipmentSetLocations(name)
-	for inventoryID, location in next, locations do
-		local itemLink = GetInventoryItemLink('player', inventoryID)
-		if(itemLink and select(3, GetItemInfo(itemLink)) == 5) then
-			-- legendary item found, manually replace it with the item from the new set
+		-- BUG: legion legendaries will halt the set equipping if the user is swapping
+		-- different slotted legendaries beyond the 1/2 equipped limit
+		local locations = GetEquipmentSetLocations(name)
+		for inventoryID, location in next, locations do
+			local itemLink = GetInventoryItemLink('player', inventoryID)
+			if(itemLink and select(3, GetItemInfo(itemLink)) == 5) then
+				-- legendary item found, manually replace it with the item from the new set
+				local action = EquipmentManager_EquipItemByLocation(location, inventoryID)
+				if(action) then
+					EquipmentManager_RunAction(action)
+					locations[inventoryID] = nil
+				end
+			end
+		end
+
+		-- Equip remaining items through _RunAction to avoid blocking from UseEquipmentSet
+		for inventoryID, location in next, locations do
 			local action = EquipmentManager_EquipItemByLocation(location, inventoryID)
 			if(action) then
 				EquipmentManager_RunAction(action)
-				locations[inventoryID] = nil
 			end
-		end
-	end
-
-	-- Equip remaining items through _RunAction to avoid blocking from UseEquipmentSet
-	for inventoryID, location in next, locations do
-		local action = EquipmentManager_EquipItemByLocation(location, inventoryID)
-		if(action) then
-			EquipmentManager_RunAction(action)
 		end
 	end
 end
